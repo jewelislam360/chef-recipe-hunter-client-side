eeb9999d61549ee225b4772a11061f40cc7cccb3
jewelislam360 <jewelislam.it360@gmail.com>
2023-05-04 15:15:09 +0600

    privet raout add

---
+++ src/Raout/PrivetRout.jsx
@@ -0,0 +1,15 @@
+import React, { useContext } from 'react';
+import { AuthContext } from '../Providers/AuthProvider';
+import { Navigate } from 'react-router-dom';
+
+const PrivetRout = ({children}) => {
+    const {user} = useContext(AuthContext)
+    if(user){
+        return children;
+
+    }
+    return <Navigate to="/login" replace={true}></Navigate>
+    
+};
+
+export default PrivetRout;
